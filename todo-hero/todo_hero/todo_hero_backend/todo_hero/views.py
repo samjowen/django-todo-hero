b54be4a9ef06de3fba3504d729a9d51690aa9cc8
Samuel Owen <SamHirael@gmail.com>
2024-05-21 20:59:26 +0100

    feat: add TodoListCreate view for Todo model

---
+++ todo-hero/todo_hero/todo_hero_backend/todo_hero/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoListCreate(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
