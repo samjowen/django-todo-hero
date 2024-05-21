aabef227d638a346e4ee7f43f33c2957fec2b780
Samuel Owen <SamHirael@gmail.com>
2024-05-21 22:26:12 +0100

    fix: Remove unused dotenv package and fix .env

--- todo-hero-frontend/src/config.ts
+++ todo-hero-frontend/src/config.ts
@@ -1,6 +1,5 @@
-import 'dotenv/config'
-
 function getEnv(key: string): string {
+    console.log(process.env)
     const value = process.env[key];
     if (!value) {
         throw new Error(`Missing env var: ${key}`);
@@ -9,7 +8,7 @@ function getEnv(key: string): string {
 }
 
 const CONFIG = {
-    TODOHERO_API_URL: getEnv('TODOHERO_API_URL'),
+    TODOHERO_API_URL: getEnv('REACT_APP_API_URL'),
 };
 
 export default CONFIG;
