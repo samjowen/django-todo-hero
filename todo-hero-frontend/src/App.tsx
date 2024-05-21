589c0438bbbcd5b9ffb6a127b6bcd772d5e482bb
Samuel Owen <SamHirael@gmail.com>
2024-05-21 22:01:19 +0100

    refactor: Update App component to use flex layout for todo list and form (scaffolding ultra basic layout)

--- todo-hero-frontend/src/App.tsx
+++ todo-hero-frontend/src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col h-screen w-screen">
+      <section className="w-full h-1/2" id="todo-list">
+        Todo List
+      </section>
+
+      <section className="w-full h-1/2" id="todo-form">
+        Todo Form
+      </section>
     </div>
   );
 }
