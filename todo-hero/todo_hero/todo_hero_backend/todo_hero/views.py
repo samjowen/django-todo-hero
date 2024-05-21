008c1a5ce1204b271a80d8184ce7900ff79b0895
Samuel Owen <SamHirael@gmail.com>
2024-05-21 21:00:38 +0100

    feat: Refactor TodoListCreate view to use ListCreateAPIView

--- todo-hero/todo_hero/todo_hero_backend/todo_hero/views.py
+++ todo-hero/todo_hero/todo_hero_backend/todo_hero/views.py
@@ -3,6 +3,7 @@ from .models import Todo
 from .serializers import TodoSerializer
 
 
+# ListCreateAPIView already handles validation and error responses by default.
 class TodoListCreate(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
