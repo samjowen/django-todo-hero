23947ba5c7f1e276cf6e87a69e232c4ca960bd32
Samuel Owen <SamHirael@gmail.com>
2024-05-21 22:28:04 +0100

    feat: Add title field to Todo model

--- todoHero/todohero/models.py
+++ todoHero/todohero/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Todo(models.Model):
     id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255, blank=False, default="")
     task = models.CharField(max_length=255)
     is_completed = models.BooleanField(default=False)
 
