1e4fdf385a8251cae77f46cff82124bfb7ef4842
Samuel Owen <SamHirael@gmail.com>
2024-05-21 21:35:24 +0100

    feat: Add Card primitive component for displaying content

---
+++ todo-hero-frontend/src/components/primitives/Card/Card.tsx
@@ -0,0 +1,42 @@
+import React, { ComponentProps, FunctionComponent } from "react";
+
+type DivProps = ComponentProps<"div">;
+
+type CardTitleProps = {
+  children: React.ReactNode;
+} & DivProps;
+
+const CardTitle = ({ children, ...props }: CardTitleProps) => {
+  return (
+    <div
+      {...props}
+      className="text-2xl font-bold text-gray-800 mb-4 pb-2 border-b-2 border-gray-200"
+    >
+      {children}
+    </div>
+  );
+};
+
+const CardContent: FunctionComponent<DivProps> = ({ children, ...props }) => {
+  return (
+    <div {...props} className="text-base text-gray-600">
+      {children}
+    </div>
+  );
+};
+
+const Card: FunctionComponent<DivProps> & {
+  Title: typeof CardTitle;
+  Content: typeof CardContent;
+} = ({ children, ...props }) => {
+  return (
+    <div {...props} className="p-4 bg-white rounded shadow-lg">
+      {children}
+    </div>
+  );
+};
+
+Card.Title = CardTitle;
+Card.Content = CardContent;
+
+export default Card;
